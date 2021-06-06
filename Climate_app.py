df7101cb67a0bc8ed0fd409e27c39f9b437e1391
cphyland <court.hyland@gmail.com>
2021-06-06 11:01:17 -0500

    push

--- Climate_app.py
+++ Climate_app.py
@@ -6,3 +6,11 @@ from sqlalchemy.orm import Session
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify, render_template
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Station = Base.classes.station
+
+last_data_point = dt.date(2017, 8, 23)
+year_ago = last_data_point - dt.timedelta(days=365)
